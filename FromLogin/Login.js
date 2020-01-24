043cce39de3354065a7d2feab87d85967b502668
Rafli20 <raflifarhans06@gmail.com>
2020-01-24 17:16:00 +0700

    Tambah Foto

--- FromLogin/Login.js
+++ FromLogin/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
     View,
     Text,
@@ -17,16 +17,20 @@ import image from '../image/logo.png'
 
 console.disableYellowBox = (true)
 
-export default class Login extends React.Component {
+class Login extends Component {
+    // State-Nya
     constructor() {
         super()
         this.state = {
             showPass: true,
             press: false,
-            role: true
+            role: true,
+            Username: '',
+            Password: ''
         }
     }
 
+    // Function Icons
     showPass = () => {
         if (this.state.press == false) {
             this.setState({ showPass: false, press: true })
@@ -34,7 +38,21 @@ export default class Login extends React.Component {
             this.setState({ showPass: true, press: false })
         }
     }
+
+    // Function Login
+    Login = () => {
+        var {
+            Username,
+            Password
+        } = this.state
+        if (Username === "Rafli" && Password === "rafli123") {
+            this.props.navigation.navigate('Home')
+        } else {
+            alert('Error')
+        }
+    }
     render() {
+        // Splash
         setTimeout(() => {
             this.setState({ role: false })
         }, 3000);
@@ -49,33 +67,40 @@ export default class Login extends React.Component {
         }
         return (
             <ScrollView>
+
                 <View style={styles.backgroundContainer}>
+
+                    {/* Logo */}
                     <View style={styles.logoContainer}>
                         <Image source={logo} style={styles.logo} />
                         <Text style={styles.logoText}> REACT NATIVE </Text>
                     </View>
 
+                    {/* Username */}
                     <View style={styles.inputContainer}>
                         <Icon name='account' size={28} color={'gray'}
                             style={styles.inputIcon}
                         />
-                        <TextInput
+                        <TextInput onSubmitEditing={this.Login}
                             style={styles.inputUser}
                             placeholder={'Username'}
+                            onChangeText={(Text) => this.setState({ Username: Text })}
                             placeholderTextColor='gray'
                             underlineColorAndroid='transparent'
 
                         />
                     </View>
 
+                    {/* Password */}
                     <View style={styles.inputContainer}>
                         <Icons name='lock'
                             size={28} color={'gray'}
                             style={styles.inputIcon}
                         />
-                        <TextInput
+                        <TextInput onSubmitEditing={this.Login}
                             style={styles.inputPass}
                             placeholder={'Password'}
+                            onChangeText={(Pass) => this.setState({ Password: Pass })}
                             secureTextEntry={this.state.showPass}
                             placeholderTextColor='gray'
                             underlineColorAndroid='transparent'
@@ -89,14 +114,16 @@ export default class Login extends React.Component {
                         </TouchableOpacity>
                     </View>
 
+                    {/* Button Login */}
                     <View style={{ alignItems: 'center' }}>
                         <TouchableOpacity style={styles.btnLogin}
-                            onPress={() => this.props.navigation.navigate("Home")}>
+                            onPress={this.Login}>
                             <Text style={styles.text}>
                                 LOGIN
                             </Text>
                         </TouchableOpacity>
                     </View>
+
                 </View>
             </ScrollView>
 
@@ -104,3 +131,4 @@ export default class Login extends React.Component {
     }
 }
 
+export default Login;
